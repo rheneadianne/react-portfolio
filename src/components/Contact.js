1e1acaac13d066f6bd4df7e2e102006ae922b3e8
rheneadianne <rheneadianne@gmail.com>
2022-10-08 04:15:41 -0400

    contact added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,18 +17,42 @@ function Contact() {
             validated ? setErrorModalState('Your email is invalid. Please try again') : setErrorModalState('')
         } else {
             if (!event.target.value.length) {
-                setErrorModalState(`${event.target.name} cannot be blank!`);
-              } else {
+                setErrorModalState(`Fields cannot be left blank!`);
+            } else {
                 setErrorModalState('');
-              }
+            }
         }
 
         if (!errorModalState) {
             setContactState({ ...contactState, [event.target.name]: event.target.value })
         }
     }
+
+    return (
+        <section class="m-5">
+            <h2>Contact Me!</h2>
+            <form onSubmit={handleFormSubmit}>
+                <div class="form-group">
+                    <label for="contactName">Your Name</label>
+                    <input type="text" class="form-control" id="contactName" aria-describedby="contactName" defaultValue={contactName} placeholder="Enter your name." onBlur={handleFormReset}/>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="email" defaultValue={email} placeholder="Enter email" onBlur={handleFormReset}/>
+                </div>
+                <div class="form-group">
+                    <label for="body">Message</label>
+                    <textarea class="form-control" id="body" rows="3" defaultValue={body} placeholder="Enter your message (please note this is frontend ONLY). Please email me @ rheneadianne@gmail.com for any contact." onBlur={handleFormReset}></textarea>
+                </div>
+                {errorModalState && (
+          <div>
+            <p class="bg-danger m-5">{errorModalState}</p>
+          </div>
+        )}
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </section>
+    )
 }
 
 export default Contact;
-
-//ill deal with the html in a bit
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Resume() {
   return (
-    <section>
+    <section class="m-5">
       <h1>Resume</h1>
-      <a href= './../assets/pdfs\rheneaResume.pdf' download> Download my resume!</a>
+      <a class="btn btn-info" href= './../assets/pdfs/rheneaResume.pdf' download="rheneaResume.pdf"> Download my resume!</a>
     </section>
   );
 }
