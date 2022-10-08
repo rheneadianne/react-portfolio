66551636c0acd55492f44202ac8d400f872bdc05
rheneadianne <rheneadianne@gmail.com>
2022-10-08 01:09:20 -0400

    added array of portfolio items as objects

---
+++ src/components/Portfolio.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+
+const myStuff = [
+    {
+        title: 'IBetter',
+        desc: 'Track your habits on a server, anywhere, everywhere',
+        repo: 'https://github.com/Vinyldude8896/iBetter/',
+        live: 'https://ibetter.herokuapp.com/',
+        img: './assets/images/iBetter.png'
+    },
+    {
+        title: 'Vent',
+        desc: 'Anonymously post your thoughts to the digital void. Vent and shout your thoughts to the world.',
+        repo: 'https://github.com/rheneadianne/vent',
+        live: 'https://challenge-vent.herokuapp.com/',
+        img: './assets/images/demoGif.gif'
+    },
+    {
+        title: 'Eat, Drink, Relax',
+        desc: 'Interactive webapp to help choose a random recipe, cocktail, and movie',
+        repo: 'https://github.com/rheneadianne/EatDrinkRelax',
+        live: 'https://rheneadianne.github.io/EatDrinkRelax/',
+        img: './assets/images/Eat.png'
+    },
+    {
+        title: 'Code Quiz',
+        desc: ' Frontend Text your knowledge quiz',
+        repo: 'https://rheneadianne.github.io/code-quiz/',
+        live: 'https://github.com/rheneadianne/code-quiz',
+        img: './assets/images/web-api-code-quiz.gif'
+    },
+    {
+        title: 'Weather Dashboard',
+        desc: 'Simple weather dashboard created using <a href="https://openweathermap.org/">OpenWeatherAPI</a>',
+        repo: 'https://github.com/rheneadianne/weather-dashboard',
+        live: 'https://rheneadianne.github.io/weather-dashboard',
+        img: './assets/images/Weather_Dashboard.gif'
+    },
+    {
+        title: 'Work Day Scheduler',
+        desc: 'Simple daily planner to plan your work hours for the day',
+        repo: 'https://github.com/rheneadianne/Work-Day-Scheduler',
+        live: 'https://rheneadianne.github.io/Work-Day-Scheduler/',
+        img: './assets/images/scheduler.gif"'
+    }
+]
+
+// ill do the html later one second
